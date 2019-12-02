b169b28e17e1afdfd00b21918a8ed72f26ce7f62
kbgeptupsi <cristianalarcon109@gmail.com>
2019-12-02 17:24:02 -0500

    Add struct of object

--- Parking.cpp
+++ Parking.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
 using name space std;
+//ESTRUCTURA CARRO
+struct CARRO{
+char placa[6];
+char modelo[20];
+char color[20];
+int puertas;
+float valorparqueocarro=1000;//1000 pesos por hora
+}
+
+//ESTRUCTURA PERSONA
+struct MOTOCICLETAS{
+char placa[6];
+char modelo[20];
+char color[20];
+float valorparqueomoto=500;//500 pesos por hora
+}
+
+//ESTRUCTURA PERSONA
+struct PERSONA{
+char nombre[40];
+char apellido[40];
+char sex;
+long id;
+//obtener fecha de ingreso
+//obtener fecha de salida
+float pago;
+}
+
+//ESTRUCTURA SITIO APARACADO
+struct SITIONORMAL{
+char tipo [20];
+}
+
+//ESTRUCTURA SITIO APARCADO PREMIUM
+struct SITIOVIP{
+char tipo[20];
+float encargo;
+}
+
 int main(){
 	
 }
